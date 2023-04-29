2eb1eadb5b0a60b381aebf2942c90586092f95ce
f020202 <111269736+f020202@users.noreply.github.com>
2023-04-30 01:31:29 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if score<101 and score>89:
+    print("A")
+elif score<90 and score>79:
+    print("B")
+elif score<80 and score>69:
+    print("C")
+elif score<70 and score>59:
+    print("D")
+else:
+    print("F")
