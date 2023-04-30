86a6eb50e9dc5b931e1452d26fdb3aeae4ee8bd7
f020202 <111269736+f020202@users.noreply.github.com>
2023-04-30 23:13:56 +0900

    Update A＋B － 7.py

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,5 +1,6 @@
 t = int(input())
-
-for i in range(1, t+1):  # 1부터 t까지
-    a, b = map(int, input().split())
-    print(f'Case #{i}: {a+b}')
+for i in range(1,t+1):
+    a,b = input().split()
+    a = int(a)
+    b = int(b)
+    print("Case #"+ str(i) + ": " + str(a+b))
