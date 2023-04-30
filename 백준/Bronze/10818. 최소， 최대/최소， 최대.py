8071cddd8bb430a833e49dcc7e5488eccc005083
f020202 <111269736+f020202@users.noreply.github.com>
2023-05-01 00:03:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 424 ms, Memory: 155736 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,10 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+max = n_list[0]
+min = n_list[0]
+for i in n_list:
+    if i>max:
+        max = i
+    if i<min:
+        min = i
+print(min, max)
