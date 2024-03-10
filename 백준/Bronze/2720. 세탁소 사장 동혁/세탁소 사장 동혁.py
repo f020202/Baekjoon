2eaf701d1d03e2da293729c2ce949decc3693ca6
혜주 <111269736+f020202@users.noreply.github.com>
2024-03-10 10:32:40 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,19 +1,11 @@
 iter = int(input())
-for _ in range (iter):
+coin = [25, 10, 5, 1]
+
+for _ in range(iter):
     rest = int(input())
-    a = b= c= d =0
-    
-    if rest > 0:
-        a = rest//25
-        rest = rest - a*25
-    if rest > 0:
-        b = rest // 10
-        rest = rest - b * 10
-    if rest > 0:
-        c = rest // 5
-        rest = rest - c * 5
-    if rest > 0:
-        d = rest // 1
-        rest = rest - d * 1
-        
-    print(a,b,c,d)
+    for i in coin:
+        a = rest // i
+        if rest > 0:
+            rest = rest - a * i
+        print(a, end=" ")
+    print("")
