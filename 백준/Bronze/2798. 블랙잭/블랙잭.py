156c858d2c84b2db343a9140ecedf1d78409e84a
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-03-24 13:35:04 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 37492 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,12 @@
 n,m = map(int,input().split())
 card = list(map(int,input().split()))
-sum = []
+sum_list = []
 
 for i in range(n-2):
     for j in range(i+1,n-1):
         for k in range(j+1,n):
-            sum.append(card[i]+card[j]+card[k])
+            sum = card[i]+card[j]+card[k]
+            if sum <= m:
+                sum_list.append(sum)
 
-result = []
-
-for i in range(len(sum)):
-    if sum[i] <= m:
-        result.append(sum[i])
-
-print(max(result))
+print(max(sum_list))
