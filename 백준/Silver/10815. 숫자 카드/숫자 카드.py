fad7aea6ff16519cd93ac114c5cecb9ffb06d635
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-04-01 14:13:39 +0900

    [Silver V] Title: 숫자 카드, Time: 668 ms, Memory: 121536 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr1 = set(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for value in arr2:
+    if value in arr1:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
