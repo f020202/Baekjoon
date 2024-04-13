d436d15611adf0561e0197a5e3d7067b721e2c64
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-04-13 23:28:04 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 55092 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,15 @@
 from collections import deque
 
 n = int(input())
+# n부터 1까지 큐에 넣기
 card = [x for x in range(n,0,-1)]
 card = deque(card)
 
 while len(card) > 1:
+    # pop으로 맨뒤에 있는거 삭제하기
+    # 큐로 만든 후에 pop한거기때문에 시간초과 안뜸
     card.pop()
+    # 큐 오른쪽으로 한칸씩 이동
     card.rotate(1)
 
 print(*card)
