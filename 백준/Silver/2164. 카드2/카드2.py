60837953eb1bee16604741a822d58f93b5f75add
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-04-13 23:25:25 +0900

    [Silver IV] Title: 카드2, Time: 224 ms, Memory: 55092 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,13 +1,11 @@
 from collections import deque
 
 n = int(input())
-card = [x for x in range(1,n+1)]
+card = [x for x in range(n,0,-1)]
 card = deque(card)
 
 while len(card) > 1:
-    # 맨 앞에있는거 삭제
-    card.popleft()
-    # 왼쪽으로 옮김 (맨앞 원소는 맨뒤로감)
-    card.rotate(-1)
+    card.pop()
+    card.rotate(1)
 
 print(*card)
