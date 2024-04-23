4dc0ccc9d894523103ae269eb693ad9dabae864f
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-04-23 11:48:30 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,6 +1,6 @@
 def solution(n):
     num = []
-    for i in range(1,n+1):
+    for i in range(1,n//2+1):
         if n%i == 0:
             num.append(i)
-    return sum(num)
+    return sum(num)+n
