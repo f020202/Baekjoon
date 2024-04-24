6e88e7fb5029c07abb1abef574b647f9632a361c
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-04-24 23:29:54 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,10 +1,7 @@
 def solution(n):
-    result = 0
+    result = -1
     for i in range(1,n+1):
         if i*i == n:
             result = (i+1)**2
             break
-    if result == 0:
-        return -1
-    else:
-        return result
+    return result
