b361f3b0eec6e244cd774d03583f03f2620db8d7
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-05-11 23:45:35 +0900

    [level 1] Title: 행렬의 덧셈, Time: 57.96 ms, Memory: 22.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    arr = []
+    for i in range(len(arr1)):
+        tmp = []
+        for j in range(len(arr1[i])):
+            tmp.append(arr1[i][j]+arr2[i][j])
+        arr.append(tmp)
+    return arr
+            
