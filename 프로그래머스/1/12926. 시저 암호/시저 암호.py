7167799db8f5a00015ecef4bf031a2e9a173e4f7
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-05-20 00:01:05 +0900

    [level 1] Title: 시저 암호, Time: 1.84 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -3,16 +3,19 @@ def solution(s, n):
     arr = []
     for char in s:
         char = ord(char)
+        # 대문자   
         if char > 64 and char < 91:
             if char + n > 90:
                 arr.append(n+char-26)
             else:
                 arr.append(char+n)
+        # 소문자
         elif char > 96 and char < 123:
             if char + n > 122:
                 arr.append(n-122+char+96)
             else:
                 arr.append(char+n)
+        # 띄어쓰기
         elif char == 32:
             arr.append(32)
     arr = [chr(x) for x in arr]
