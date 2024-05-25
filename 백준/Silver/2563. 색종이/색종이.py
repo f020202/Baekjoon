637ffc07edc3f4743968af7c8bde8f3fc32623c4
Hyeju Jung <111269736+f020202@users.noreply.github.com>
2024-05-25 23:25:09 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [[0]*100 for _ in range(100)]
+
+for _ in range(n):
+    x, y = map(int,input().split())
+    for i in range(x,x+10):
+        for j in range(y,y+10):
+            arr[i][j] = 1
+
+sum = 0
+for k in range(100):
+    sum += arr[k].count(1)
+print(sum)
